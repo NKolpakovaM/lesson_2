56d399245dd85b3f72c102d991d7a43c89ecdb23
Наташа <KolpakovaNM@yandex.ru>
2025-02-14 17:40:51 +0500

    lesson 2 task 1 done

---
+++ src/main/kotlin/lesson2_task1.kt
@@ -0,0 +1,10 @@
+package org.example
+
+fun main() {
+    val mark1 = 3.0
+    val mark2 = 4.0
+    val mark3 = 3.0
+    val mark4 = 5.0
+    val medium = (mark1 + mark2 + mark3 + mark4) / 4
+    print("Среднее значение успеваевости $medium")
+}
