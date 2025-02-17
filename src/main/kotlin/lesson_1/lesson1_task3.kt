845dec572fdf8450ee060bc6d9872fe7b756416f
Fridon <fedin.mail@gmail.com>
2025-02-17 18:15:12 +0300

    KS1-3 Решена задача 3 из урока 1 (форматирования времени полета Гагарина)

---
+++ src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -0,0 +1,17 @@
+package lesson_1
+
+fun main() {
+
+    val toSpaceYear = 1961
+    val toSpaceHour = 9
+    val toSpaceMinute = 7
+    val outSpaceHour = 10
+    val outSpaceMinute = 55
+
+    println("Gagarin flight time was:")
+    println("in $toSpaceYear year")
+    println("at $toSpaceHour hours")
+    println("and $toSpaceMinute minutes")
+    print("Gagarin landing time was : $outSpaceHour:$outSpaceMinute")
+
+}
