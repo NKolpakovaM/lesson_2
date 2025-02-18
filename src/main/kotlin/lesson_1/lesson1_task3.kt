cb4d43cdb83e4779bfd0562f3e1e57e478434481
NKolpakovaM <KolpakovaNM@yandex.ru>
2025-02-18 21:50:56 +0500

    KS-1-3 исправлены ошибки

---
+++ src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -0,0 +1,15 @@
+fun main() {
+    val toSpaceYear : Int = 1961
+    var hour : Int = 9
+    var minute : Int = 7
+    println("Gagarin launch time was:")
+    println("in $toSpaceYear year")
+    println("at $hour hours")
+    println("$minute minutes")
+    println(" ")
+    hour = 10
+    minute = 55
+    println("Gagarin landing time was:")
+    println("in $toSpaceYear year")
+    print("at $hour:$minute")
+}
