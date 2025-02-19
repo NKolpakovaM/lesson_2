b9515a03f0137e9ce7d00797f9c07d603e8c405f
NKolpakovaM <KolpakovaNM@yandex.ru>
2025-02-19 17:38:06 +0500

    KS-3-2 задача выполнена

---
+++ src/main/kotlin/lesson_3/lesson3_task2.kt
@@ -0,0 +1,11 @@
+fun main () {
+    val firstName = "Татьяна"
+    var surname = "Андреева"
+    val lastName = "Сергеевна"
+    var age: Int
+    age = 20
+    println("В возрасте $age лет имя пользователя: $surname $firstName $lastName")
+    age = 22
+    surname = "Сидорова"
+    println("В возрасте $age лет имя пользователя: $surname $firstName $lastName")
+}
