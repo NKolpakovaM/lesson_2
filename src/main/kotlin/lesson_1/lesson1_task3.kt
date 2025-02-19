65b2f658910712466580a13594a49995db3f5c9d
NKolpakovaM <KolpakovaNM@yandex.ru>
2025-02-19 22:40:56 +0500

    KS-1-3 задача исправлена

--- src/main/kotlin/lesson_1/lesson1_task3.kt
+++ src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -5,9 +5,7 @@ fun main() {
     println("Gagarin launch time was:")
     println("in $toSpaceYear year")
     println("at 0$hour:0$minute")
-
     println(" ")
-
     hour = 10
     minute = 55
     println("Gagarin landing time was:")
